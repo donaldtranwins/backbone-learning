2029cc7281e0bd764bc9e6fefb0c6a47ed324822
Donald J. Tran <donald.tran@kushbottles.com>
2018-07-11 18:26:34 -0700

    Completed: 3-02

---
+++ Lynda.com/03/03-02/start/js/views/singleFlowerView.js
@@ -0,0 +1,10 @@
+// Namespace our flowerApp
+var app = app || {};
+
+// The view for a single model view, which is one flower
+app.singleFlowerView = Backbone.View.extend({
+
+    tagName: "article",
+    className: "flowerListItem",
+
+});
