9985f69d78caab6e0a2171622d4889929719c871
Donald J. Tran <donald.tran@kushbottles.com>
2018-07-11 18:42:26 -0700

    Completed: 3-03

--- Lynda.com/03/03-03/start/js/views/singleFlowerView.js
+++ Lynda.com/03/03-03/start/js/views/singleFlowerView.js
@@ -6,4 +6,16 @@ app.singleFlowerView = Backbone.View.extend({
   tagName: "article",
   className: "flowerListItem",
 
+    //tells the View.template where to get the HTML template
+    template: _.template( $("#flowerElement").html() ),
+
+    render: function() {
+        //grab data from the model to be rendered in a template by the view
+        var flowerTemplate = this.template(this.model.toJSON());
+        //takes the $el created, the one specified in line 6/7 with tagName and className, and pass in a template
+        this.$el.html(flowerTemplate);
+        return this;
+    }
+
+
 });
